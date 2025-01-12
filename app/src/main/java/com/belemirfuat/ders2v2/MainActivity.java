2e67eeb6c457e3a1324306888123715d398654d3
BelemirFuat <mtshbz@gmail.com>
2025-01-12 19:47:47 +0300

    ayarlar okunup uygulama başında toasta yazıldı

--- app/src/main/java/com/belemirfuat/ders2v2/MainActivity.java
+++ app/src/main/java/com/belemirfuat/ders2v2/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.database.Cursor;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
